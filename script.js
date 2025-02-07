bf650b2c57fec2ce4074d7b9429411a986829312
camila <mariacamilaluango@gmail.com>
2025-02-07 00:34:04 -0500

    definimos variables

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const citasMedicas = ["2024-03-15","2024-03-20","2024-03-15", "2024-04-01"];
 function citaCercana(fechaPaciente){
     if (citasMedicas.length === 0)
         return null;
+    let fechaPacienteDate = new Date(fechaPaciente);
+    let citaCercana = citasMedicas[0];
+    let diferenciaMin = Math.abs(new Date(citaCercana) - fechaPacienteDate);
 }
