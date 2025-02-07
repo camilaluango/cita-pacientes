9b0fd8a26c40f348e946dac974141ce26356404b
camila <mariacamilaluango@gmail.com>
2025-02-07 00:45:58 -0500

    lanzar resultado

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const citasMedicas = ["2024-03-15","2024-03-20","2024-03-15", "2024-04-01"];
+const fechaPaciente = "2024-03-22"
 
-function citaCercana(fechaPaciente){
+function encontrarcitaCercana(fechaPaciente){
     if (citasMedicas.length === 0)
         return null;
+    
     let fechaPacienteDate = new Date(fechaPaciente);
     let citaCercana = citasMedicas[0];
     let diferenciaMin = Math.abs(new Date(citaCercana) - fechaPacienteDate);
@@ -19,5 +21,8 @@ function citaCercana(fechaPaciente){
         }
 
     }
+    return citaCercana;
 
 }
+
+console.log(encontrarcitaCercana(fechaPaciente));
